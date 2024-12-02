206c1beefb87c809eba6a62f5f65fbf303879cdd
aman10rasheed <amanrasheedpro@gmail.com>
2024-12-02 22:05:47 +0530

    OAuth buttom added

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,56 @@
-import { SafeAreaView, Text, View } from "react-native";
-
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/inputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 const SignIn = () => {
+  const [form, setForm] = useState({
+    eamil:"",
+    password:""
+  })
   return (
-    <SafeAreaView>
-      <View>
-        <Text>SignIn</Text>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="Relative w-full h-[250px]">
+          <Image className="w-full h-[250px]" source={images.signUpCar} />
+          <Text className="text-4xl font-JakartaSemiBold text-black bottom-5 left-5">
+            Welcome
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label={"Email"}
+            placeholder="Enter name"
+            icon={icons.email}
+            value={form.eamil}
+            onChangeText={(value) => setForm({ ...form, eamil: value })}
+          />
+          <InputField
+            label={"Password"}
+            placeholder="Enter name"
+            icon={icons.lock}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+            secureTextEntry
+          />
+          <CustomButton
+            title="Sign In"
+            // onPress={onSignUpPress}
+            className="mt-6"
+          />
+            <OAuth/>
+          <Link
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            Don't have an account?{" "}
+            <Text className="text-primary-500">Sign Up</Text>
+          </Link>
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,64 @@
-import { SafeAreaView, Text, View } from "react-native";
-
+import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
+import { icons, images } from "@/constants";
+import InputField from "@/components/inputField";
+import { useState } from "react";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+import OAuth from "@/components/OAuth";
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name:"",
+    eamil:"",
+    password:""
+  })
   return (
-    <SafeAreaView>
-      <View>
-        <Text>SignUp</Text>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="Relative w-full h-[250px]">
+          <Image className="w-full h-[250px]" source={images.signUpCar} />
+          <Text className="text-4xl font-JakartaSemiBold text-black bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label={"Name"}
+            placeholder="Enter name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label={"Email"}
+            placeholder="Enter name"
+            icon={icons.email}
+            value={form.eamil}
+            onChangeText={(value) => setForm({ ...form, eamil: value })}
+          />
+          <InputField
+            label={"Password"}
+            placeholder="Enter name"
+            icon={icons.lock}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+            secureTextEntry
+          />
+          <CustomButton
+            title="Sign Up"
+            // onPress={onSignUpPress}
+            className="mt-6"
+          />
+            <OAuth/>
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            Already have an account?{" "}
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
